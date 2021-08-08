e839b368f1124aa2dbaa6e4feaf4d85587a46840
ajsaule <49604000+ajsaule@users.noreply.github.com>
2021-08-08 11:12:32 +1000

    fixing routing epxress

--- src/App.js
+++ src/App.js
@@ -1,33 +1,31 @@
-import React, { useEffect, useState } from 'react'
-import btc from './btc.png'
-import './App.css'
+import React, {useEffect, useState} from "react";
+import btc from "./btc.png";
+import "./App.css";
 
 function App() {
-
   const [coinData, setCoinData] = useState({
     symbol: null,
     prevAsk: null,
     ask: null,
-    delta: null
-  })
+    delta: null,
+  });
 
   const checkCrypto = (coin) => {
-    const apiURL = `http://localhost:5000/${coin}`
-    
+    const apiURL = `${window.location.href}/${coin}`;
+
     fetch(apiURL)
       .then((response) => response.json())
       .then((data) => {
-        console.log(data)
+        console.log(data);
         setCoinData({
           symbol: data.buy,
           prevAsk: coinData.ask,
           ask: data.ask,
-          delta: (coinData.ask - coinData.prevAsk) / coinData.prevAsk * 100
-        })
+          delta: ((coinData.ask - coinData.prevAsk) / coinData.prevAsk) * 100,
+        });
       })
-      .catch((err) => console.log('The errors are as follows..', err))
-  }
-
+      .catch((err) => console.log("The errors are as follows..", err));
+  };
 
   return (
     <div className="App">
@@ -35,17 +33,26 @@ function App() {
         <div className="Padding"></div>
         <img src={btc} className="App-logo" alt="btc" />
         <h1> Check your crypto prices </h1>
-        <a onClick={() => checkCrypto('BTC')} className="App-link"> Bitcoin </a>
-        <a onClick={() => checkCrypto('ETH')} className="App-link"> Ethereum </a>
-        <a onClick={() => checkCrypto('XRP')} className="App-link"> Ripple </a>
+        <a onClick={() => checkCrypto("BTC")} className="App-link">
+          {" "}
+          Bitcoin{" "}
+        </a>
+        <a onClick={() => checkCrypto("ETH")} className="App-link">
+          {" "}
+          Ethereum{" "}
+        </a>
+        <a onClick={() => checkCrypto("XRP")} className="App-link">
+          {" "}
+          Ripple{" "}
+        </a>
         <section>
           <p> Token symbol: {coinData.symbol}</p>
           <p> Ask price: ${Math.round(coinData.ask)} USD </p>
           <p> % change: {coinData.delta}% </p>
-        </section>      
+        </section>
       </header>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
